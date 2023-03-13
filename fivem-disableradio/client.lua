b11478b3f1e5836f05afc521bcf99eadcd64ada1
IByalvaro <127791602+IByalvaro@users.noreply.github.com>
2023-03-13 21:10:28 +0100

    Add files via upload

---
+++ fivem-disableradio/client.lua
@@ -0,0 +1,16 @@
+
+ESX = exports["es_extended"]:getSharedObject()
+local PlayerData                = {}
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+            if IsPedInAnyVehicle(PlayerPedId()) then
+                SetUserRadioControlEnabled(false)
+                if GetPlayerRadioStationName() ~= nil then
+                SetVehRadioStation(GetVehiclePedIsIn(PlayerPedId()),"OFF")
+                end
+        end
+    end
+end)
